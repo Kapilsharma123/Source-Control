1ce52a2a21e87ed831346c2242a2cabfc59e9d79
ABC <ABC@gmail.com>
2022-05-11 16:00:00 +0530

    Initial Commit

---
+++ Source Control/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Source Control
+//
+//  Created by Htcuser on 11/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView{
+                    VStack
+                    {
+                        Rectangle().fill(Color.red).ignoresSafeArea()
+                        
+                    }
+                    
+                }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Source Control/Source_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Source_ControlApp.swift
+//  Source Control
+//
+//  Created by Htcuser on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Source_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
