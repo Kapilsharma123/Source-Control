9a917d932793b6ac930d21a251adc2af49a12a86
ABC <ABC@gmail.com>
2022-05-17 10:49:58 +0530

    test

---
+++ Source Control/ContentView2.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView2.swift
+//  Source Control
+//
+//  Created by Htcuser on 17/05/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct ContentView2: View {
+    var body: some View {
+        NavigationView{
+                    VStack
+                    {
+                        Rectangle().fill(Color.brown).ignoresSafeArea()
+                        Rectangle().fill(Color.gray).ignoresSafeArea()
+                        
+                    }
+                    
+                }
+       
+    }
+}
+
+struct ContentView2_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView2()
+    }
+}
