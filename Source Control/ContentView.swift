8dcb931c7f6984caa7ba9b9842f7aeb6ec6a8215
Kapil Sharma <KSharma@thegeneral.com>
2022-08-05 12:50:57 +0530

    reverted

--- Source Control/ContentView.swift
+++ Source Control/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
                         Rectangle().fill(Color.black).ignoresSafeArea()
 Text("Here DIG 1001 changes are initiated")
                         Rectangle().fill(Color.gray)
-                       Text("DIG1002")
+                        Text("DIG1001")
 
                     }
                     
