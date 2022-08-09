e121330cbecfef19ede5cc2a0d7b60457ec56a47
Kapil Sharma <KSharma@thegeneral.com>
2022-08-09 10:45:57 +0530

    no message

---
+++ Source Control/ContentView4.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView4.swift
+//  Source Control
+//
+//  Created by Htcuser on 17/05/22.
+//
+
+
+import Foundation
+import SwiftUI
+
+struct ContentView4: View {
+    var body: some View {
+        NavigationView{
+                    VStack
+                    {
+                        Text("DIG1001 changes")
+                        Text("ContentView4 added")
+                        Text("Content4")
+                        
+                    }
+                    
+                }
+       
+    }
+}
+
+struct ContentView4_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView4()
+    }
+}
+
